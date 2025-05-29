56fbcff08237f809f0c006c1eaad4aef6d3c9dc0
Christian99909 <christian99909@gmail.com>
2025-05-29 10:49:01 -0600

    Añadir Dapper y mejoras en validación de formularios
    Se ha integrado Dapper y Microsoft.Data.SqlClient para facilitar las consultas a la base de datos.
    El controlador `TiposCuentasController` ahora incluye una cadena de conexión y una consulta a la base de datos en el método `Crear`.
    Se ha implementado una validación personalizada en el modelo `TipoCuenta` para asegurar que la primera letra del nombre sea mayúscula.
    Además, se han mejorado las etiquetas y campos de entrada en la vista `Crear.cshtml` para utilizar la validación de ASP.NET.
    Se ha añadido un archivo de configuración `appsettings.Development.json` con la cadena de conexión para el entorno de desarrollo y se ha creado la clase `PrimeraLetraMayusculaAttribute` para la validación.

--- ManejoPresupuesto/Controllers/TiposCuentasController.cs
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -1,20 +1,37 @@
-﻿using ManejoPresupuesto.Models;
+﻿using Dapper;
+using ManejoPresupuesto.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace ManejoPresupuesto.Controllers
 {
     public class TiposCuentasController : Controller
     {
-
-        public IActionResult Crear() 
+        private readonly string connectionString;
+        public TiposCuentasController(IConfiguration configuration)
         {
-            return View();
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public IActionResult Crear()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var query = connection.Query("SELECT 1").FirstOrDefault();
+            
+            }
+                return View();
         
         }
 
         [HttpPost]
         public IActionResult Crear(TipoCuenta tipoCuenta) 
         {
+
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
             return View();
         
         }
